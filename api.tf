1b8439d01a5b06db6bc7a88d00b837e57e27b581
Krishnatejab17 <17bdevops@gmail.com>
2025-12-04 06:21:01 +0000

    working now

--- api.tf
+++ api.tf
@@ -25,3 +25,11 @@ resource "aws_apigatewayv2_stage" "stage" {
 output "invoke_url" {
   value = aws_apigatewayv2_api.api.api_endpoint
 }
+
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
